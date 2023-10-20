cb0dfe0bde7b2593cecba232b7f193a8a8dea154
5selny <5selny@email.com>
2023-10-20 19:01:44 +0900

    유저 접근 권한 테스트

---
+++ src/test/java/shop/allegro/controller/ItemControllerTest.java
@@ -0,0 +1,32 @@
+package shop.allegro.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ItemControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    @DisplayName("상품 권한 페이지 등록 테스트")
+    @WithMockUser(username = "admin", roles = "ADMIN")
+    // 회원 이름이 admin 이고 role 이 ADMIN 인 유저가 로그인된 상태로 테스트할 수 있게 해주는 어노ㅔ이션
+    public void itemFormTest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/admin/item/new"))  //상품 등록 페이지에 get 요청
+                .andDo(print())     // 요청과 응답 메세지를 확인할 수 있도록 콘솔창에 출력
+                .andExpect(status().isOk());    // 응답 상태 코드가 정상인지 확인
+    }
+}
